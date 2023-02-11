802995ea875479f66e537ea569ee51fce54c6eff
valeriy <valeriybaranov.elitel@gmail.com>
2023-02-11 12:42:00 +0300

    hw12-oop: добавить добавить класс хелпер для работы с

--- hw06-annotations/src/main/java/ru/otus/Main.java
+++ hw06-annotations/src/main/java/ru/otus/Main.java
@@ -1,43 +1,18 @@
 package ru.otus;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) {
         runTest(TestFramework.class);
     }
 
-    private static void runTest(Class<TestFramework> clazz) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        // allocates memory for 10 integers
-        Map<Class, Method> annotationMethods = new HashMap<>();
-        for (final Method declaredMethod : clazz.getDeclaredMethods()) {
-            if (declaredMethod.isAnnotationPresent(Before.class)) {
-                Annotation annotInstance = declaredMethod.getAnnotation(Before.class);
-                System.out.println(annotInstance);
-                Constructor<TestFramework> constructor = clazz.getConstructor();
-                TestFramework object = constructor.newInstance();
-                Method method = object.getClass().getDeclaredMethod(declaredMethod.getName());
-                annotationMethods.put(Before.class, method);
-                method.setAccessible(true);
-                method.invoke(object);
-            }
+    private static void runTest(Class clazz) {
+        List<ArrayList<Method>> sortedMethods = TestFrameworkHelper.getSortedMethods(clazz);
+        for (ArrayList<Method> object : sortedMethods) {
+            object.forEach((Method method) -> TestFrameworkHelper.callMethod(TestFramework.class, method));
         }
-
-//        for (HashMap: annotationMethods.entrySet()) {
-        //                if (declaredMethod.isAnnotationPresent(Before.class)) {
-        //                Annotation annotInstance = declaredMethod.getAnnotation(Before.class);
-        //                System.out.println(annotInstance);
-        //                Constructor<TestFramework> constructor = clazz.getConstructor();
-        //                TestFramework object = constructor.newInstance();
-        //                Method method = object.getClass().getDeclaredMethod(declaredMethod.getName());
-        //                annotationMethod.put(Before.class, method);
-        //                method.setAccessible(true);
-        //                method.invoke(object);
-        annotationMethods.entrySet().forEach(Map.Entry<Class, Method>::getClass);
     }
 }
--- hw06-annotations/src/main/java/ru/otus/TestFramework.java
+++ hw06-annotations/src/main/java/ru/otus/TestFramework.java
@@ -1,21 +1,39 @@
 package ru.otus;
 
 public class TestFramework {
-    @Before
-    public void before() {
-        System.out.print("Метод: before. ");
+    @After
+    public void after1() {
+        System.out.print("Метод: after1 ");
         System.out.println("Экземпляр тестового класса: " + Integer.toHexString(hashCode()));
     }
 
     @Test
-    void test() {
-        System.out.println("Метод: test ");
+    public void test1() {
+        System.out.print("Метод: test1 ");
+        System.out.println("Экземпляр тестового класса: " + Integer.toHexString(hashCode()));
+    }
+
+    @Before
+    public void before1() {
+        System.out.print("Метод: before1 ");
+        System.out.println("Экземпляр тестового класса: " + Integer.toHexString(hashCode()));
+    }
+
+    @Test
+    public void test2() {
+        System.out.print("Метод: test2 ");
         System.out.println("Экземпляр тестового класса: " + Integer.toHexString(hashCode()));
     }
 
     @After
-    public void after() {
-        System.out.println("Метод: after ");
+    public void after2() {
+        System.out.print("Метод: after2 ");
+        System.out.println("Экземпляр тестового класса: " + Integer.toHexString(hashCode()));
+    }
+
+    @Before
+    public void before2() {
+        System.out.print("Метод: before2 ");
         System.out.println("Экземпляр тестового класса: " + Integer.toHexString(hashCode()));
     }
 }
